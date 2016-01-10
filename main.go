6d8131e314bf933d75803f03d378d117da007146
Deepak Kaul <Deepak.Kaul@nasa.gov>
2016-01-10 11:32:14 -0500

    updating to use different cli lib

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"github.com/codegangsta/cli"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	app := cli.NewApp()
+	app.Name = "goweb"
+	app.Usage = "Simple file web server"
+	app.Version = "1.0.0"
+	var addr string = ":8080"
+	var dir string = "."
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "a, addr",
+			Value:       addr,
+			Usage:       "server address",
+			Destination: &addr,
+		},
+		cli.StringFlag{
+			Name:        "d, dir",
+			Value:       dir,
+			Usage:       "directory to serve",
+			Destination: &dir,
+		},
+	}
+	app.Action = func(cmd *cli.Context) {
+		log.Println("Serving", dir, "on", addr)
+		http.ListenAndServe(addr, http.FileServer(http.Dir(dir)))
+	}
+	app.Run(os.Args)
 }
